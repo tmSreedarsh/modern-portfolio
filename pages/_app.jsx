06701fadab90a3c7054862e12a4e0890d2c3a4c7
Sreedarsh T M <tmsreedarsh@gmail.com>
2026-02-16 11:16:38 +0530

    Add both tracking tools

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,12 @@
 import { AnimatePresence, motion } from "framer-motion";
 import { useRouter } from "next/router";
 
+// 1. Visitor Tracker (Analytics)
+import { Analytics } from "@vercel/analytics/react";
+
+// 2. Speed Tracker (Speed Insights) - NEW
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 import Layout from "../components/Layout";
 import Transition from "../components/Transition";
 
@@ -17,6 +23,11 @@ function MyApp({ Component, pageProps }) {
           <Component {...pageProps} />
         </motion.div>
       </AnimatePresence>
+      
+      {/* Tracker Tools: Both running together */}
+      <Analytics />
+      <SpeedInsights />
+      
     </Layout>
   );
 }
